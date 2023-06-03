b5e8353a13c4f5f93161364fa1aaf6ec78866d6b
jornkbz <jornkbz@gmail.com>
2023-06-03 12:01:06 +0200

    adaptando_codigo_streamlit
    Primera toma de contacto con streamlit, adaptación para ejecución y estudio

--- Streamlit_app/pages/2_AAP_FORECAST_MODEL.py
+++ Streamlit_app/pages/2_AAP_FORECAST_MODEL.py
@@ -3,10 +3,30 @@ Modelo de predicción para Forecast
 """
 import streamlit as st
 from pages.page_styler.page_style import ForecastSetup
-from forecast.forecast import input_data
+from forecast.forecast import ForecastModel
+
 
 # Setup de los datos de la página
 ForecastSetup()
 
-# Introducción de datos
-input_data()
+# MODELO FORECAST
+# Instancia de clase para introducir datos y predecir
+forecast_model = ForecastModel()
+
+# Tabs con las secciones para introducir datos y mostrar resultados
+tab_forecast, tab_crew, tab_flight, tab_res = st.tabs([
+    "DATOS CLIMATOLOGICOS", "DATOS TRIPULACIÓN", "DATOS VUELO",
+    ":blue[RESULTADOS]"])
+
+
+with tab_forecast:
+    forecast_model.data_forecast()
+
+with tab_crew:
+    forecast_model.data_crew()
+
+with tab_flight:
+    forecast_model.data_flight()
+
+with tab_res:
+    forecast_model.res_data()
