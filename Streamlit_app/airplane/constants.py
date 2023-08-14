60b6d9fc3df855ea3d7fea522944b8428e4279a0
Pablo Ruiz <pabloruizve@gmail.com>
2023-08-14 12:24:38 +0200

    Primera versión Pycaret Airplane
    Actualización del modelo y primera versión de PyCaret

---
+++ Streamlit_app/airplane/constants.py
@@ -0,0 +1,18 @@
+"""
+Module created to contain the constant values
+"""
+
+# Directorio con fuente de datos
+SOURCE_DIRECTORY = 'source/airplane'
+# Diccionario para mapeo de datos categóricos
+MAPPED_DIC = f'{SOURCE_DIRECTORY}/mapped_dictionary.csv'
+# Leyenda de las columnas
+COL_NAME_DICT = 'source/column_info.csv'
+# Información de las columnas
+HELP_INFO = 'airplane/help_columns.json'
+# Normalizado y escalado
+SCALER_1 = f'{SOURCE_DIRECTORY}/scaler_c31_c151.joblib'
+# Modelo de predicción
+MODEL = 'Airplane_model'
+# Directorio de resultados
+RESULTS = 'source/results.json'
--- Streamlit_app/pages/3_AAP_AIRPLANE_MODEL.py
+++ Streamlit_app/pages/3_AAP_AIRPLANE_MODEL.py
@@ -1,11 +1,34 @@
 """
 Modelo de predicción para Airplane
 """
-# import streamlit as st
-# from pages.page_styler.page_style import AirplaneSetup
-# from airplane.airplane import *
+import streamlit as st
+from pages.page_styler.page_style import AirplaneSetup
+from airplane.airplane import AirplaneModel
 
 # Setup de los datos de la página
-# AirplaneSetup()
+AirplaneSetup()
 
-# Introducción de datos
+# MODELO AIRPLANE
+# Instancia de clase para introducir datos y predecir
+airplane_model = AirplaneModel()
+
+# Tabs con las secciones para introducir datos y mostrar resultados
+# tab_forecast, tab_crew, tab_flight, tab_aux, tab_res = st.tabs([
+tab_general, tab_motor, tab_vuelo, tab_res = st.tabs([
+    "DATOS GENERALES", "DATOS DEL MOTOR", "DATOS DE VUELO", ":blue[RESULTADOS]"])
+#    "DATOS AUXLIARES", ":blue[RESULTADOS]"])
+
+with tab_general:
+    airplane_model.data_general()
+
+with tab_motor:
+    airplane_model.data_motor()
+
+with tab_vuelo:
+    airplane_model.data_vuelo()
+
+#with tab_aux:
+#    airplane_model.data_aux()
+
+with tab_res:
+    airplane_model.res_data()
