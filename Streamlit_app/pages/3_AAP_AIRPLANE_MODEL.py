8a97453a617ee739a1f80931b2284008a8a373d6
Pablo Ruiz <pabloruizve@gmail.com>
2023-08-25 21:09:44 +0200

    Update 3_AAP_AIRPLANE_MODEL.py

--- Streamlit_app/pages/3_AAP_AIRPLANE_MODEL.py
+++ Streamlit_app/pages/3_AAP_AIRPLANE_MODEL.py
@@ -1,9 +1,10 @@
 """
 Modelo de predicción para Airplane
 """
+
 import streamlit as st
 from pages.page_styler.page_style import AirplaneSetup
-from airplane.airplane import AirplaneModel
+from airplane.airplane import *
 
 # Setup de los datos de la página
 AirplaneSetup()
